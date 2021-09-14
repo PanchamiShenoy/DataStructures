0aa9e32d5fab4c0625c6d9556afd4f8913291cdf
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-14 23:12:37 +0530

    banking cash counter is added

---
+++ src/com/yml/dataStructures/BankingCashCounterMain.java
@@ -0,0 +1,9 @@
+package com.yml.dataStructures;
+
+public class BankingCashCounterMain {
+
+	public static void main(String args[]) {
+		BankingCashCounter bcc =new BankingCashCounter();
+		bcc.addPerson();
+	}
+}
